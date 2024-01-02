ad477173b6098ff12f2173ffb6fd342a7906e175
ayamaruu <113888875+ayamaruu@users.noreply.github.com>
2024-01-03 00:30:15 +0900

    testba

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Ayaka Murakoshi
+# SPDX-Linense-Identifier: BSD-3-Clause
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -6,5 +9,6 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
 cat /tmp/mypkg.log |
 grep 'Listen: 10'
